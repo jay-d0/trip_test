6f083f9e0724bdf0afd4daf568eeb7f1694cc17b
golf5885 <golf5885@yonsei.ac.kr>
2023-06-06 21:52:10 +0900

    save

--- src/Character.js
+++ src/Character.js
@@ -5,9 +5,11 @@ import "./css/Character.css"
 function Character(){
     return(
         <div className="many_guides">
-            <Guide name="A" style = "Fast" img_url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Q82WISxpWPp5dHBTWHypFOZbRTvc0ST0xQ&usqp=CAU"></Guide>
-            <Guide name="B" style = "Slow" img_url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Q82WISxpWPp5dHBTWHypFOZbRTvc0ST0xQ&usqp=CAU"></Guide>
-            <Guide name="C" style = "Fun" img_url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Q82WISxpWPp5dHBTWHypFOZbRTvc0ST0xQ&usqp=CAU"></Guide>
+            <Guide name="김은서" style = "여유롭게 여행을 즐기는 편" img_url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Q82WISxpWPp5dHBTWHypFOZbRTvc0ST0xQ&usqp=CAU"></Guide>
+            <Guide name="김재도" style = "쇼핑 위주의 여행" img_url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Q82WISxpWPp5dHBTWHypFOZbRTvc0ST0xQ&usqp=CAU"></Guide>
+            <Guide name="민성우" style = "맛집 위주로 여행" img_url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Q82WISxpWPp5dHBTWHypFOZbRTvc0ST0xQ&usqp=CAU"></Guide>
+            <Guide name="박유찬" style = "박물관, 유적지를 좋아하는 편" img_url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Q82WISxpWPp5dHBTWHypFOZbRTvc0ST0xQ&usqp=CAU"></Guide>
+            <Guide name="서우석" style = "현지인들과 어울리기를 좋아함." img_url= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6Q82WISxpWPp5dHBTWHypFOZbRTvc0ST0xQ&usqp=CAU"></Guide>
         </div>
     )
 }
