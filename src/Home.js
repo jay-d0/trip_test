c4980ab3346a4df33d7c16efd8c866c6bbe38e93
Eunsuh Kim <eunsuhkim1@gmail.com>
2023-06-06 21:19:16 +0900

    home 및 map css 수정

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
-import './App.css';
-import { useNavigate, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './css/Home.css';
+import logoImage from './icons/home-image.png';
 
 export default function Home() {
-
   return (
-    <div>
-      <h1>YBIGTOUR</h1>
-    <Link to="./Character">
-        <button>START</button>
-    </Link>
+    <div className="home-container">
+      <div className="background-color"></div>
+      <img src={logoImage} alt="Logo" className="logo-image" />
+      <div className="content">
+        <h1>YBIGTOUR</h1>
+        <Link to="./Character">
+          <button>START</button>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/Map.js
+++ src/Map.js
@@ -1,20 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import L from 'leaflet';
+import 'leaflet-providers';
 import 'leaflet/dist/leaflet.css';
 import './css/Map.css';
+import './css/Map_Popup.css';
 
 import markerIcon from './icons/location-pin.png';
 
 function Map() {
   const { character } = useParams();
-
-  // Define the map container ID
-  const mapContainerId = 'map';
+  const mapRef = useRef(null);
 
   useEffect(() => {
     // Create the map instance
-    const map = L.map(mapContainerId, {
+    const map = L.map(mapRef.current, {
       zoomControl: false,
       dragging: false,
       doubleClickZoom: false,
@@ -23,7 +23,7 @@ function Map() {
       keyboard: false,
       tap: false,
       touchZoom: false,
-      maxBounds: [[-90, -180], [90, 180]],
+      maxBounds: [[85, -180], [-85, 180]],
       maxZoom: 2,
     });
 
@@ -31,34 +31,51 @@ function Map() {
     map.setView([0, 0], 2);
 
     // Create and add the tile layer to the map
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    L.tileLayer.provider('CartoDB.PositronNoLabels', {
+      attribution: 'Map data &copy; <a href="https://carto.com/">Carto</a>',
       noWrap: true
     }).addTo(map);
 
+
     // Create a custom icon
     const customIcon = L.icon({
-        iconUrl: markerIcon,
-        iconSize: [32, 32],
-        iconAnchor: [16, 32],
-    })
+      iconUrl: markerIcon,
+      iconSize: [32, 32],
+      iconAnchor: [16, 32],
+    });
 
-    const parisMarker = L.marker([48.8566, 2.3522], { icon: customIcon }).addTo(map);
-    const tokyoMarker = L.marker([35.6895, 139.6917], { icon: customIcon }).addTo(map);
+    // Create markers with popups
+    const parisMarker = L.marker([48.8566, 2.3522], { icon: customIcon }).addTo(map).bindPopup('파리');
+    const tokyoMarker = L.marker([35.6895, 139.6917], { icon: customIcon }).addTo(map).bindPopup('도쿄');
 
-    parisMarker.bindTooltip('파리').openTooltip();
-    tokyoMarker.bindTooltip('도쿄').openTooltip();
+    // Mouseover event handler
+    const onMouseOver = (e) => {
+      const marker = e.target;
+      marker.openPopup();
+    };
+
+    // Mouseout event handler
+    const onMouseOut = (e) => {
+      const marker = e.target;
+      marker.closePopup();
+    };
+
+    // Add mouseover and mouseout event listeners to markers
+    parisMarker.on('mouseover', onMouseOver);
+    parisMarker.on('mouseout', onMouseOut);
+    tokyoMarker.on('mouseover', onMouseOver);
+    tokyoMarker.on('mouseout', onMouseOut);
 
     // Clean up the map instance when the component is unmounted
     return () => {
       map.remove();
     };
-  }, [mapContainerId]);
+  }, []);
 
   return (
     <div className="map-wrapper">
-      <h1>{character}님, 여행할 도시를 선택해 주세요</h1>
-      <div id={mapContainerId} className="map-container"></div>
+      <h1 className="map-title">{character}님, 여행할 도시를 선택해 주세요</h1>
+      <div id="map" className="map-container" ref={mapRef}></div>
     </div>
   );
 }
